7a805f16bb5f4ee5b52b7fde37c2dbd8d3cfa440
vladimir <tarskij-vladimir@yandex.ru>
2024-01-04 15:41:27 +0300

    added date create

---
+++ date_create.py
@@ -0,0 +1,20 @@
+def input_name():
+    return input('введите имя: ')
+
+def input_surname():
+    return input('введите фамилию: ')
+
+def input_patronymic():
+    return input('введите отчество: ')
+
+def input_phone():
+    return input('введите номер телефона: ')
+
+def input_address():
+    return input('введите адрес: ')
+
+
+
+
+
+
